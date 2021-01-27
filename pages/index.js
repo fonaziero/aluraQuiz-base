86c667ebe8547a7216047515515ea5bf8d564d65
Victor <victorcarnevale@yahoo.com.br>
2021-01-27 20:38:56 -0300

    first commit

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,58 @@
 import styled from 'styled-components'
+import db from '../db.json'
+import Widget from '../src/components/Widget/index'
+import QuizBackground from '../src/components/QuizBackground/index'
+import Footer from '../src/components/Footer/index'
+import GitHubCorner from '../src/components/GitHubConer/index'
+
+
+
+// const BackgroundImage = styled.div`
+//   background-image: url(${db.bg}); 
+//   flex: 1;
+//   background-size: cover;
+//   background-position: center;
+// `;
+
+export const QuizContainer = styled.div`
+  width: 100%;
+  max-width: 350px;
+  padding-top: 45px;
+  margin: auto 10%;
+  @media screen and (max-width: 500px) {
+    margin: auto;
+    padding: 15px;
+  }
+`;
 
 const Title = styled.h1`
   font-size: 50px;
   color: ${({ theme }) => theme.colors.primary};
-`
+`;
 
 export default function Home() {
-  return <Title>My page</Title>
+  return (
+    <QuizBackground backgroundImage={db.bg}>
+      <QuizContainer>
+        <Widget>
+          <Widget.Header> 
+            <h1>Teste</h1>
+          </Widget.Header>
+          <Widget.Content>
+            <p>Teste</p>
+          </Widget.Content>
+        </Widget>
+        <Widget>
+        <Widget.Header>
+          <h1>Quiz da Galera</h1>
+        </Widget.Header>
+          <Widget.Content>
+            <p>Teste</p>
+          </Widget.Content>
+        </Widget>
+        <Footer/>
+      </QuizContainer>
+      <GitHubCorner projectUrl="https://github.com/"/>
+    </QuizBackground>
+  );
 }
